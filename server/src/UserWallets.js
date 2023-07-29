aa2905a57d34565a5c566b0e440b05cfdd5fca55
Brooke Smith <brooke@oehmsmith.com>
2023-07-29 16:05:57 +1000

    Changed UserWallets class
    * Made multiple classes
    * Using Map() instead of Object
    * Fully tested

--- server/src/UserWallets.js
+++ server/src/UserWallets.js
@@ -1,7 +1,38 @@
 const Cryptography = require('./Cryptography')
 
+/**
+ * Class for Users and their Wallets.  Exploded out in to 3 classes to make expanding the wallet fields easier.
+ */
+class Wallets {
+    balance = 0
+
+    constructor(balance = 0) {
+        this.balance = balance;
+    }
+}
+
 class UserWallets {
-    userWallets = {}; // Users to multiple wallets
+    wallets = new Map<Wallets>(null);    // Map of wallet addresses to Wallets
+
+    add(address, balance=0) {
+        this.wallets.set(address, new Wallet(balance))
+    }
+
+    remove(address) {
+        this.wallets.delete(address);
+    }
+
+    getAddresses() {
+        return Array.from(this.wallets.keys());
+    }
+
+    getWallets(address) {
+        return Array.from(this.wallets.get(address));
+    }
+}
+
+class Users {
+    users = new Map(); // Map of Users to UserWallets
 
     constructor() {
         this.setupDummyData();
@@ -16,60 +47,71 @@ class UserWallets {
     }
 
     clear() {
-        this.userWallets = {};
+        this.users.clear();
     }
 
+    /**
+     *
+     * @return Array of usernames
+     */
     getUsers() {
-        return Object.keys(this.userWallets);
+        return Array.from(this.users.keys());
     }
 
+    /**
+     * @param user
+     * @return Map of user wallets
+     */
     getUserWallets(user) {
         if (!user) {
             throw Error("Must have user arg");
-        };
-        return this.userWallets[user];
+        }
+        return this.users.get(user);
     }
 
     /**
      * @param user
      * @param publicKey
      * @param balance
-     * @return boolean - if user existed
-     */
+     * @exception Error if existing user attempted to be added
+     **/
     createUpdateUserWallet(user, publicKey, balance = 0) {
         let userAlreadyExisted = false;
         if (!(user && publicKey) ) {
             throw Error("Must have user and wallet args");
         }
 
-        if (!this.userWallets.hasOwnProperty(user)) {
+        if (!this.users.has(user)) {
             this.addUser(user);
         } else {
             userAlreadyExisted = true;
         }
-        this.userWallets[user][publicKey] = {balance: balance};
+        const allWallets = this.getAllWallets();
+        const userWallets = this.users.get(user)
+        if (allWallets.has(publicKey)) {
+            throw new Error(`Wallet addresses must be unique - ${publicKey} already used.`)
+        }
+        userWallets.set(publicKey, {balance})
         return userAlreadyExisted;
     }
 
     /**
      * @param user
-     * @returns {boolean} - if user added (didn't already exist)
+     * @exception Error if existing user attempted to be added
      */
     addUser(user) {
-        if (!this.userWallets.hasOwnProperty(user)) {
-            // this.users.push(user);
-            this.userWallets[user] = {};
-            return true;
+        if (this.users.has(user)) {
+            throw new Error(`Attempting to add existing user: ${user}`)
         }
-        return false;
+        this.users.set(user, new Map());
     }
 
 
     getUser(user) {
-        if (!this.userWallets.hasOwnProperty(user)) {
+        if (!this.users.has(user)) {
             return null;
         }
-        return this.userWallets[user];
+        return this.users.get(user);
     }
 
     /**
@@ -79,14 +121,38 @@ class UserWallets {
      * @return wallet w public key and balance or null if no user or user hasn't wallet
      */
     getUserWallet(user, wallet) {
-        if (!this.userWallets.hasOwnProperty(user)) {
+        const userWallets = this.users.get(user);
+        if (!userWallets) {
             return null;
         }
-        if (!this.userWallets[user].hasOwnProperty(wallet)) {
-            return null;
-        }
-        return this.userWallets[user][wallet];
+        return userWallets.get(wallet);
+    }
+
+    /**
+     * @return Map of wallets
+     */
+    getAllWallets() {
+        let allWallets =  {};
+        this.users.forEach((userValue, userKey) => {
+            allWallets = {...allWallets, ...Object.fromEntries(userValue)};
+            // userValue.forEach((userWalletValue, userWalletKey) => {
+            //     allWallets.push(Object.fromEntries(userWalletValue));
+            // });
+        });
+        return new Map(Object.entries(allWallets));
+    }
+
+    /**
+     *
+     * @return Array of wallet addresses
+     */
+    getAllWalletAddressess() {
+        const allWalletAddresses =  [];
+        this.users.forEach((userValue, userKey) => {
+            allWalletAddresses.push(...Array.from(userValue.keys()));
+        });
+        return allWalletAddresses;
     }
 }
 
-module.exports = UserWallets;
+module.exports = {Users, UserWallets, Wallets};
--- server/src/UserWallets.spec.js
+++ server/src/UserWallets.spec.js
@@ -1,63 +1,118 @@
-const UserWallets = require("./UserWallets");
+const { all } = require("express/lib/application")
+const UserWallets = require("./UserWallets").UserWallets;
+const Users = require("./UserWallets").Users;
+const Wallets = require("./UserWallets").Wallets;
 
 describe('UserWallets', () => {
-    const uw = new UserWallets();
-    const user = 'zzzz';
-    const wallet='0xABC';
+    const users = new Users();
+    const user1 = 'user_1';
+    const wallet11='0xABC';
+    const wallet12='0xDEF';
+    const user2 = 'user_2';
+    const wallet21 = '0x123'
+    const wallet22 = '0x456'
 
     beforeEach(() => {
-        uw.clear();
+        users.clear();
     });
 
     describe('AddUser', () => {
-        it('duplicates', () => {
-            expect(uw.getUsers()).toHaveLength(0);
-            expect(uw.addUser(user)).toEqual(true);
-            expect(uw.addUser(user)).toEqual(false);
-            expect(uw.getUsers()).toHaveLength(1);
+        it(`should not be able to add user twice 1`, () => {
+            users.addUser(user1);
+            expect(() => users.addUser(user1)).toThrowError();
+        })
+        it(`should not be able to add user twice 2`, () => {
+            users.createUpdateUserWallet(user1, 'xxx');
+            expect(() => users.addUser(user1)).toThrowError();
         })
     });
 
     describe('getUser', () => {
        it('empty wallets', () => {
-           uw.addUser(user);
-           expect(uw.getUser(user)).toEqual({});
+           users.addUser(user1);
+           expect(users.getUser(user1)).toEqual(new Map());
        })
 
         it('one wallet', () => {
-            uw.addUser(user);
-            uw.createUpdateUserWallet(user, wallet, 99);
-            expect(uw.getUser(user)).toEqual({w1: {balance: 99}});
+            users.addUser(user1);
+            users.createUpdateUserWallet(user1, wallet11, 99);
+            const asObj = Object.fromEntries(users.getUser(user1))
+            expect(asObj).toEqual({"0xABC": {balance: 99}});
         })
         it('two wallets', () => {
-            uw.addUser(user);
-            uw.createUpdateUserWallet(user, wallet, 99);
-            uw.createUpdateUserWallet(user, wallet+'2', 999);
-            expect(uw.getUser(user)).toEqual({w1: {balance: 99}, w12: {balance: 999}});
+            users.addUser(user1);
+            users.createUpdateUserWallet(user1, wallet11, 99);
+            users.createUpdateUserWallet(user1, wallet11+'2', 999);
+            const asObj = Object.fromEntries(users.getUser(user1))
+            expect(asObj).toEqual({"0xABC": {balance: 99}, "0xABC2": {balance: 999}});
         })
     });
 
     describe('Wallets', () => {
         it('updateUserWallet as update', () => {
-            expect(uw.addUser(user)).toEqual(true);
-            expect(Object.keys(uw.getUserWallets(user))).toHaveLength(0);
-            uw.createUpdateUserWallet(user, wallet, 99);
-            const userWallets = uw.getUserWallets(user)
-            expect(Object.keys(userWallets)).toHaveLength(1);
-            expect(userWallets[wallet].balance).toEqual(99)
+            expect(users.getUser(user1)).toEqual(null);
+            users.createUpdateUserWallet(user1, wallet11, 99);
+            const theUserWallets = users.getUserWallets(user1)
+            expect(theUserWallets.size).toEqual(1);
+            expect(theUserWallets.get(wallet11).balance).toEqual(99)
         });
 
         it('updateUserWallet as new user should succeed', () => {
-            const newUser = 'aaa';
-            const newWallet = '0x123'
-
             // Don't do this in this test
             // expect(uw.addUser(newUser)).toEqual(true);
             // expect(Object.keys(uw.getUserWallets(newUser))).toHaveLength(0);
-            uw.createUpdateUserWallet(newUser, newWallet, 666);
-            const userWallets = uw.getUserWallets(newUser)
-            expect(Object.keys(userWallets)).toHaveLength(1);
-            expect(userWallets[newWallet].balance).toEqual(666)
+            users.createUpdateUserWallet(user2, wallet21, 666);
+            const theUserWallets = users.getUserWallets(user2)
+            expect(theUserWallets.size).toEqual(1);
+            expect(theUserWallets.get(wallet21).balance).toEqual(666)
         });
-    })
+
+        it('multiple users, one wallet each', () => {
+            users.createUpdateUserWallet(user1, wallet11, 555);
+            users.createUpdateUserWallet(user2, wallet21, 666);
+            expect(users.getUsers()).toHaveLength(2);
+            expect(users.getUser(user1).get(wallet11).balance).toEqual(555)
+            expect(users.getUser(user2).get(wallet21).balance).toEqual(666)
+        })
+
+        it('multiple users, multi wallets each, no duplicates', () => {
+            users.createUpdateUserWallet(user1, wallet11, 550);
+            users.createUpdateUserWallet(user1, wallet12, 555);
+            users.createUpdateUserWallet(user2, wallet21, 660);
+            users.createUpdateUserWallet(user2, wallet22, 666);
+            expect(users.getUsers()).toHaveLength(2);
+            expect(users.getAllWalletAddressess()).toHaveLength(4);
+            expect(users.getUser(user1).get(wallet11).balance).toEqual(550)
+            expect(users.getUser(user1).get(wallet12).balance).toEqual(555)
+            expect(users.getUser(user2).get(wallet21).balance).toEqual(660)
+            expect(users.getUser(user2).get(wallet22).balance).toEqual(666)
+            const allWallets = users.getAllWallets();
+            expect(allWallets.size).toEqual(4);
+            expect(allWallets.get(wallet11).balance).toEqual(550);
+            expect(allWallets.get(wallet12).balance).toEqual(555);
+            expect(allWallets.get(wallet21).balance).toEqual(660);
+            expect(allWallets.get(wallet22).balance).toEqual(666);
+        });
+        it('multiple users, multi wallets each, duplicates', () => {
+            // Wallets must have unique address
+            try {
+                users.createUpdateUserWallet(user1, wallet11, 550);
+                users.createUpdateUserWallet(user1, wallet12, 555);
+                users.createUpdateUserWallet(user2, wallet11, 660);
+                users.createUpdateUserWallet(user2, wallet22, 666);
+                throw new Error("test should not reach here");
+            } catch (e) {
+                // Even though no wallets were added for user, they should still be included as a user
+                expect(users.getUsers()).toHaveLength(2);
+                expect(users.getAllWalletAddressess()).toHaveLength(2);
+                expect(users.getUser(user1).get(wallet11).balance).toEqual(550)
+                expect(users.getUser(user1).get(wallet12).balance).toEqual(555)
+                expect(users.getUserWallets(user2).size).toEqual(0);
+                const allWallets = users.getAllWallets();
+                expect(allWallets.size).toEqual(2);
+                expect(allWallets.get(wallet11).balance).toEqual(550);
+                expect(allWallets.get(wallet12).balance).toEqual(555);
+            }
+        });
+    });
 })
