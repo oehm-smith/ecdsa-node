f5e80027b463398c08cc1eb5bff1404e725f8f97
Brooke Smith <brooke@oehmsmith.com>
2023-08-03 10:12:24 +1000

    Cleaned up index.js (main)

--- server/index.js
+++ server/index.js
@@ -1,42 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-var morgan = require('morgan')
-
+const morgan = require('morgan')
 const UserController = require("./src/UserController")
 
 const port = 3042;
 
 const app = express();
 
-// const balances = {
-//   "0x1": 100,
-//   "0x2": 50,
-//   "0x3": 75,
-// };
-//
-// app.get("/balance/:address", (req, res) => {
-//   const { address } = req.params;
-//   const balance = balances[address] || 0;
-//   res.send({ balance });
-// });
-//
-// app.post("/send", (req, res) => {
-//   const { sender, recipient, amount } = req.body;
-//
-//   setInitialBalance(sender);
-//   setInitialBalance(recipient);
-//
-//   if (balances[sender] < amount) {
-//     res.status(400).send({ message: "Not enough funds!" });
-//   } else {
-//     balances[sender] -= amount;
-//     balances[recipient] += amount;
-//     res.send({ balance: balances[sender] });
-//   }
-// });
-
-
-// -----------------------------------------------------------------
+function printRoutes() {
+  console.log(`Routes:`)
+  app._router.stack.forEach(function (r) {
+    if (r.route && r.route.path) {
+      console.log(`    ` + Object.keys(r.route.methods)[0].toUpperCase() + " " + r.route.path)
+    }
+  })
+}
 
 function main() {
   app.use(cors());
@@ -49,19 +27,7 @@ function main() {
     console.log(`Listening on port ${port}`);
   });
 
-  (() => {
-    console.log(`Routes:`)
-    app._router.stack.forEach(function (r) {
-      if (r.route && r.route.path) {
-        console.log(`    ` + Object.keys(r.route.methods)[0].toUpperCase() + " " + r.route.path)
-      }
-    })
-  })();
+  printRoutes();
 }
 
 main();
-function setInitialBalance(address) {
-  if (!balances[address]) {
-    balances[address] = 0;
-  }
-}
